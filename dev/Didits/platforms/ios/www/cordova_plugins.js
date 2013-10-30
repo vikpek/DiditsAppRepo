35503868cbcf5a8329480b57cb3b5dcc3dc03712
Viktor Pekar <vikpek@big-claptrap.home>
2013-10-30 13:04:21 +0100

    inappbrowser plugin removed

--- dev/Didits/platforms/ios/www/cordova_plugins.js
+++ dev/Didits/platforms/ios/www/cordova_plugins.js
@@ -41,13 +41,6 @@ module.exports = [
         "merges": [
             "navigator.notification"
         ]
-    },
-    {
-        "file": "plugins/org.apache.cordova.inappbrowser/www/InAppBrowser.js",
-        "id": "org.apache.cordova.inappbrowser.InAppBrowser",
-        "clobbers": [
-            "window.open"
-        ]
     }
 ]
 });
